4e86ffdde6e729520989902666c4ff90f6a2df6c
Dohyung Park <dohyung.prk@gmail.com>
2021-04-25 00:36:08 +0900

    add keyset_path setting

--- nopeek/settings.py
+++ nopeek/settings.py
@@ -5,6 +5,7 @@ In settings.py,
 NOPEEK_SETTINGS = {
     "CIPHER_MODULE": "tink.daead",
     "KMS_INTEGRATION_CLIENT": "tink.integration.gcpkms.GcpKmsClient",
+    "KEYSET_PATH": "gcp-kms://projects/tink-examples/locations/global/keyRings/foo/cryptoKeys/bar",
 }
 """
 
@@ -15,6 +16,7 @@ from .utils import import_callable
 DEFAULT_SETTINGS = {
     "CIPHER_MODULE": import_callable("tink.aead"),
     "KMS_INTEGRATION_CLIENT": None,
+    "KEYSET_PATH": None,
 }
 
 nopeek_settings = getattr(settings, "NOPEEK_SETTINGS", DEFAULT_SETTINGS)
