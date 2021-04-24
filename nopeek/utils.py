a7622a1574157886a92a69cb7ce0517fc1472286
Dohyung Park <dohyung.prk@gmail.com>
2021-04-25 00:09:44 +0900

    inject django dependency

--- nopeek/utils.py
+++ nopeek/utils.py
@@ -1,7 +1,8 @@
 """Utilities"""
 
-from importlib import import_module
 from typing import Callable, Union
+
+from django.utils.module_loading import import_string
 
 
 def import_callable(path_or_callable: Union[Callable, str]) -> Callable:
@@ -15,7 +16,12 @@ def import_callable(path_or_callable: Union[Callable, str]) -> Callable:
     """
     if hasattr(path_or_callable, "__call__"):
         return path_or_callable
+    elif isinstance(path_or_callable, str):
+        try:
+            return import_string(path_or_callable)
+        except ImportError as e:
+            message = f"Could not import {path_or_callable}. {e.__class__.__name__}: {e}."
+            raise ImportError(message)
     else:
-        assert isinstance(path_or_callable, str)
-        package, attr = path_or_callable.rsplit(".", 1)
-        return getattr(import_module(package), attr)
+        message = f"Passed parameter {path_or_callable} is not callable or string."
+        raise TypeError(message)
