5fc665afbfaeeb2a88af1170e787e199a1d40d38
Dohyung Park <dohyung.prk@gmail.com>
2021-05-29 19:52:30 +0900

    add test settings

--- nopeek/tests/test_settings.py
+++ nopeek/tests/test_settings.py
@@ -1,9 +1,11 @@
 """Test Django Setting Module"""
+from pathlib import Path
 
 INSTALLED_APPS = ("nopeek",)
 
 NOPEEK_SETTINGS = {
-    "CIPHER_CLASS": "nopeek.crypto.DefaultCipher",
+    "CIPHER_CLASS": "nopeek.crypto.KMSClientCipher",
     "CIPHER_TEMPLATE": "AES256_GCM",
-    "KEYSET_PATH": "./keyset.json",
+    "KEY_URI": "aws-kms://arn:aws:kms:us-east-2:235739564943:key/3ee50705-5a82-4f5b-9753-05c4f473922f",
+    "KMS_CREDENTIALS": str(Path(__file__).resolve().parent.joinpath("test_aws_credentials.txt")),
 }
